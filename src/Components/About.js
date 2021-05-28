b7ec5635cce4a056d86a564fa34362197198e4b6
nadiarasul <nadia.rasul@gmail.com>
2021-05-27 21:18:40 -0400

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,6 +5,21 @@ function About(props) {
 			<h1>About this app</h1>
 			<p>
 				This is an app for planning your running outfit based on the weather.
+				The icons are from{" "}
+				<a
+					href="https://thenounproject.com/"
+					target="_blank"
+					rel="noopener noreferrer">
+					The Noun Project
+				</a>
+				. Special thanks to{" "}
+				<a
+					href="https://www.momentist.com/"
+					target="_blank"
+					rel="noopener noreferrer">
+					Garry Waller
+				</a>{" "}
+				for design.
 			</p>
 		</div>
 	);
